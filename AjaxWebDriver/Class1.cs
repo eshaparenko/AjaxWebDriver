164d90973617ca4282b09232a2eb718dce5cc0a1
Eugene Shaparenko <eshaparenko@lohika.com>
2017-02-24 11:54:34 +0200

    first commit

---
+++ AjaxWebDriver/Class1.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjaxWebDriver
+{
+    [TestFixture]
+    public class Class1
+    {
+        IWebDriver driver;
+        [Test]
+        public void testAjax()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.w3schools.com/xml/ajax_intro.asp";
+
+            string originalText = driver.FindElement(By.CssSelector("div#demo>h2")).Text;
+
+            driver.FindElement(By.CssSelector("div#demo>button")).Click();
+
+            bool isReady = WaitTool.WaitForJqueryProcessing(driver, 15);
+            if (isReady)
+            {
+                string afterAJAX_Call_Text = driver.FindElement(By.CssSelector("div#demo>h1")).Text;
+                Console.WriteLine("Before "+ originalText);
+                Assert.False(originalText.Equals(afterAJAX_Call_Text, StringComparison.OrdinalIgnoreCase));
+                Console.WriteLine("After " + afterAJAX_Call_Text);
+            }
+            else
+            {
+                Assert.Fail("Verify Failed: AJAX XMLHttpRequest is not ready");
+            }
+
+
+            
+        }
+    }
+}
---
+++ AjaxWebDriver/WaitTool.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AjaxWebDriver
+{
+    class WaitTool
+    {
+        public static int DEFAULT_WAIT_4_ELEMENT = 7;
+        public static int DEFAULT_WAIT_4_PAGE = 12;
+
+        public static IWebElement WaitForElement(IWebDriver driver, By by, int timeOutInSeconds)
+        {
+            IWebElement element;
+            try
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOutInSeconds));
+                element = wait.Until(ExpectedConditions.ElementIsVisible(by));
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(DEFAULT_WAIT_4_ELEMENT);
+                return element;
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.StackTrace);
+            }
+            return null;
+        }
+        public static IWebElement WaitForElementPresent(IWebDriver driver, By by, int timeOutInSeconds)
+        {
+            IWebElement element;
+            try
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOutInSeconds));
+                element = wait.Until(ExpectedConditions.ElementExists(by));
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(DEFAULT_WAIT_4_PAGE);
+                return element;
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.StackTrace);
+            }
+            return null;
+        }
+        public static bool WaitForJqueryProcessing(IWebDriver driver, int timeOutInSeconds)
+        {
+            bool jQcondition = false;
+            try
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeOutInSeconds));
+                jQcondition = wait.Until(d=>(bool)(d as IJavaScriptExecutor).ExecuteScript("return $.active == 0"));
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(DEFAULT_WAIT_4_PAGE);
+
+                return jQcondition;
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.StackTrace);
+            }
+            return jQcondition;
+        }
+
+    }
+}
