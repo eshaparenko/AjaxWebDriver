793d16d293843bc0a065cb45fd1e65b9e32df65b
Eugene Shaparenko <eshaparenko@lohika.com>
2017-02-28 18:42:14 +0200

    Add Factorial

---
+++ C_Sharp_Sandbox/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Factorial(10));
+        }
+
+        static int Factorial(int number)
+        {
+            if (number <= 1)
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+    }
+}
